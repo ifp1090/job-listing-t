519616c6b94a9743d780b73cf5c5aacfb4ae5f84
norekcn@gmail.com <norekcn@gmail.com>
2017-05-03 19:23:13 +0800

    seeds20

--- db/migrate/20170125024912_add_category_name_to_job.rb
+++ db/migrate/20170125024912_add_category_name_to_job.rb
@@ -1,5 +1,5 @@
 class AddCategoryNameToJob < ActiveRecord::Migration[5.0]
   def change
-  	add_column :jobs, :category_name, :string, :default => "技术"
+  	add_column :jobs, :category_name, :string, 
   end
 end
